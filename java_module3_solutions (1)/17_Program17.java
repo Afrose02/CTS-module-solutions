7baa85e9588805cefcf492ba3e9414f4ad0e20f1
Afrose02 <afrosemehazafin@gmail.com>
2025-07-12 19:33:16 +0530

    Update and rename 17_Program17.java to 17_ClassandObjectCreation.java

--- java_module3_solutions (1)/17_Program17.java
+++
@@ -1,7 +0,0 @@
-// Java Program 17
-// This is a placeholder program for problem 17.
-public class Program17 {
-    public static void main(String[] args) {
-        System.out.println("Executing Program 17");
-    }
-}
