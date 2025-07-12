c90b67c1b7f9fa34f504b2891dc1ad07f0565a33
Afrose02 <afrosemehazafin@gmail.com>
2025-07-12 20:41:00 +0530

    Update and rename 37_Program37.java to 37_Using javap toInspectBytecode.java

---
+++ java_module3_solutions (1)/37_Using javap toInspectBytecode.java
@@ -0,0 +1,8 @@
+public class BytecodeDemo {
+    public void sayHello() {
+        System.out.println("Hello, Bytecode!");
+    }
+    public static void main(String[] args) {
+        new BytecodeDemo().sayHello();
+    }
+}
