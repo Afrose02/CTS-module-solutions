4c0e7f7be2355c9d98b02a0ed76de3cc0ed302f6
Afrose02 <afrosemehazafin@gmail.com>
2025-07-12 19:34:12 +0530

    Update and rename 18_Program18.java to 18_InheritanceExample.java

--- java_module3_solutions (1)/18_Program18.java
+++
@@ -1,7 +0,0 @@
-// Java Program 18
-// This is a placeholder program for problem 18.
-public class Program18 {
-    public static void main(String[] args) {
-        System.out.println("Executing Program 18");
-    }
-}
