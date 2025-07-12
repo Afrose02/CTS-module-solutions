684ce5dadbcd8234def340773609fca63b3fa535
Afrose02 <afrosemehazafin@gmail.com>
2025-07-12 20:39:24 +0530

    Update and rename 36_Program36.java to 36_HttpClientAPI.java

---
+++ java_module3_solutions (1)/36_HttpClientAPI.java
@@ -0,0 +1,13 @@
+import java.net.http.*;
+import java.net.*;
+public class HttpClientExample {
+    public static void main(String[] args) throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("https://api.github.com"))
+            .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Status: " + response.statusCode());
+        System.out.println("Body: " + response.body());
+    }
+}
